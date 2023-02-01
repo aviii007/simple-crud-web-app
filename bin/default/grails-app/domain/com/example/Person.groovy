ab994628c8ba670282f41aa6ce81748c43aa284f
aviii007 <kanojia.avi@gmail.com>
2023-02-01 16:07:47 +0530

    Added: The services and views for person

--- bin/default/grails-app/domain/com/example/Person.groovy
+++ bin/default/grails-app/domain/com/example/Person.groovy
@@ -2,6 +2,14 @@ package com.example
 
 class Person {
 
+    String name
+    String email
+    String address
+    String city
+    String country
+    String contact
+
     static constraints = {
+        email email: true
     }
 }
